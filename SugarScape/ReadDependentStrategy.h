7c974f1bf204c9f4d6bc90c0c412761e10d29339
Joseph Kehoe <Joseph.Kehoe@itcarlow.ie>
2016-02-29 15:08:45 +0000

    added strategy classes

---
+++ SugarScape/ReadDependentStrategy.h
@@ -0,0 +1,27 @@
+//
+//  ReadDependentStrategy.h
+//  SugarScape
+//
+//  Created by Joseph P Kehoe on 29/02/2016.
+//  Copyright (c) 2016 Joseph P Kehoe. All rights reserved.
+//
+
+#ifndef __SugarScape__ReadDependentStrategy__
+#define __SugarScape__ReadDependentStrategy__
+
+#include <iostream>
+#include "Strategy.h"
+
+/*!  \class  ReadDependentStrategy
+ \brief Synchronous updates where the agent reads the neighbour's state but only update their own
+ 
+ Read-Dependent Rules with synchronous updating all use from this. 
+ */
+class ReadDependentStrategy:public Strategy{
+public:
+    ReadDependentStrategy(World*);
+    virtual ~ReadDependentStrategy();
+    virtual bool run(int,int,int,Action*);
+    virtual bool concurrentRun(void);
+};
+#endif /* defined(__SugarScape__ReadDependentStrategy__) */
