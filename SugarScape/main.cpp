2a5588eddf81665934313f1773665509bd524a05
Joseph Kehoe <Joseph.Kehoe@itcarlow.ie>
2016-08-11 14:44:23 +0100

    GUI for evolution

--- SugarScape/main.cpp
+++ SugarScape/main.cpp
@@ -61,6 +61,135 @@ int cc(World*,std::string);
 int getStats(World*);
 
 int evolution(std::string);
+int evolutionGUI(std::string);
+
+int evolutionGUI(std::string fileName)
+{
+    std::ofstream outputFile(fileName,std::ios::out | std::ios::app);
+    World *theWorld = new World(50);/*!< create world and initialise it */
+    theWorld->init("log/output.log");
+    theWorld->sync();
+    /*!< Declare all possible strategies here */
+    Strategy baseStrategy(theWorld);
+    NewSweepStrategy newSweep(theWorld);
+    LineByLineStrategy lineByLine(theWorld);
+    RndAsyncStrategy rndAsync(theWorld);
+    IndependentStrategy independent(theWorld);
+    IterativeWriteStrategy iterativeWrite(theWorld);
+    ReadDependentStrategy readDependent(theWorld);
+    WriteStrategy writeDependent(theWorld);
+    /*!< Declare all rules here */
+    Growback growback(theWorld,&independent);
+    AgentMove move(theWorld,&writeDependent);
+    GarbageCollection gc(theWorld,&independent);
+    AgentDeath agentDeath(theWorld,&readDependent);
+    AgentMating agentMating(theWorld,&iterativeWrite);
+    AgentMetabolism agentMetabolism(theWorld,&independent);
+    //!
+    /*!
+     Add the rules we are using here.
+     Ordering of rules is important
+     Do death last and metabolism before replacement!!
+     */
+    /*!< Rules for Lattice are added first (before Agent Rules)*/
+    theWorld->addRule(&growback);
+    /*!< Movement Rule for Agents follow next -pick only one!*/
+    theWorld->addRule(&move);
+    theWorld->addRule(&agentMating);
+    /*!< Finally add Metabolism and (replacement or death) and finish with garbage collection*/
+    theWorld->addRule(&agentMetabolism);
+    theWorld->addRule(&agentDeath);
+    theWorld->addRule(&gc);
+
+    move.setStrategy(&newSweep);
+    //agentMating.setStrategy(&newSweep);
+    //!
+    /*!
+     Start simulation!
+     */
+    /*!<  Create the main window */
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "SFML window");
+    /*!< create viewport for displaying simulation */
+    ViewPort theGUI(&window,theWorld,std::pair<int,int>(1024, 768),std::pair<int,int>(0,0),theWorld->getSize());
+    // Set the Icon
+    sf::Image icon;
+    if (!icon.loadFromFile("resources/icon.png")) {
+        return EXIT_FAILURE;
+    }
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    // Create a graphical text to display
+    sf::Font font;
+    if (!font.loadFromFile("resources/sansation.ttf")) {
+        return EXIT_FAILURE;
+    }
+    int stepCount=0;
+    std::string counter = std::to_string(stepCount);
+    sf::Text text(counter, font, 50);
+    text.setColor(sf::Color::White);
+
+    // Load a music to play
+    sf::Music music;
+    if (!music.openFromFile("resources/nice_music.ogg")) {
+        return EXIT_FAILURE;
+    }
+
+    // Play the music -it never hurts!
+    music.play();
+    sf::Time t1 = sf::seconds(0.1f);
+    int populationSize=0;
+    float avgMetabolism=0.0;
+    float avgVision=0.0;
+    int ageGraph[10]={0,0,0,0,0,0,0,0,0,0};
+    Location* theLattice=theWorld->getLattice();
+    int size=theWorld->getSize()*theWorld->getSize();
+
+
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+
+            // Escape pressed: exit
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+                window.close();
+            }
+        }
+
+        // Clear screen
+        window.clear();
+
+        theWorld->applyRules();
+        counter = std::to_string(++stepCount);
+        text.setString(counter);
+        theGUI.draw();
+        // Draw the sprite
+        //window.draw(sprite);
+
+        // Draw the string
+        window.draw(text);
+
+        // Update the window
+        window.display();
+        if (theWorld->getAgentCount() < 300){
+            sf::sleep(t1);
+        }
+        if(theWorld->incStep()>3000) window.close();
+        outputFile << theWorld->getStep() << ","  << theWorld->getAgentCount()<< "," << theWorld->getBlueCount()
+        << std::endl;
+    }
+
+
+    delete theWorld;
+    return 1;
+
+}
 
 int evolution(std::string fileName)
 {
@@ -100,7 +229,7 @@ int evolution(std::string fileName)
     theWorld->addRule(&agentDeath);
     theWorld->addRule(&gc);
 
-    //move.setStrategy(&newSweep);
+    move.setStrategy(&newSweep);
     //agentMating.setStrategy(&newSweep);
     //!
     /*!
@@ -132,11 +261,11 @@ int evolution(std::string fileName)
         avgVision=avgVision/populationSize;
         avgMetabolism=avgMetabolism/populationSize;
         if(populationSize>0) {
-            outputFile << theWorld->getStep() << "," << populationSize << "," << avgMetabolism << "," << avgVision;
+            std::cout << theWorld->getStep() << "," << populationSize << "," << avgMetabolism << "," << avgVision;
             for (int j = 0; j < 10; ++j) {
-                outputFile << "," << ageGraph[j];
+                std::cout << "," << ageGraph[j];
             }
-            outputFile << std::endl;
+            std::cout << std::endl;
         }
     }
     delete theWorld;
@@ -517,7 +646,7 @@ int Gui(World *theWorld, float pause)
 int main(int, char const**)
 {
 
-evolution("log/evol.txt");
+evolutionGUI("log/evol.txt");
 
    // World *theWorld = nullptr;/*!< create world */
    //omp_set_num_threads(1);
